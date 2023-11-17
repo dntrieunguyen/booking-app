6989aee9981664ed1cfbecd5ca754ff91587c4ca
dntrieunguyen <dntrieunguyen@gmail.com>
2023-11-17 23:40:22 +0700

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+function Home() {
+   return (
+      <>
+         <Header></Header>
+         <main></main>
+      </>
+   );
+}
+
+export default Home;
